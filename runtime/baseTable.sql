3b329279d1f7feb6e4733acf673b97b4cd75e7af
Raj Vaida <raj.vaida@cubic.com>
2017-03-14 14:24:56 -0700

    Default Table Structure # 2

---
+++ runtime/baseTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE paypaldata ( 
+    record_id    serial PRIMARY KEY, 
+	record_stamp timestamp NOT NULL, 
+    order_number varchar(40), \
+    contact_id   varchar(40) NOT NULL, 
+    pp_string	 varchar(500) NOT NULL, 
+	pp_respo	 varchar(500) NOT NULL, 
+    pp_amount    double precision, 
+	order_amount double precision 
+); 
