0db05cb38eb8a25c988b3313ef6bcce385a02491
Raj Vaida <raj.vaida@cubic.com>
2017-03-14 18:06:14 -0700

    Improvement# Permissions and table structure

--- runtime/baseTable.sql
+++ runtime/baseTable.sql
@@ -1,10 +1,13 @@
 CREATE TABLE paypaldata ( 
     record_id    serial PRIMARY KEY, 
-	record_stamp timestamp NOT NULL, 
+	record_stamp timestamp default current_timestamp, 
     order_number varchar(40), 
-    contact_id   varchar(40) NOT NULL, 
+    contact_id   varchar(40), 
     pp_string	 varchar(500) NOT NULL, 
 	pp_respo	 varchar(500) NOT NULL, 
     pp_amount    double precision, 
 	order_amount double precision 
 ); 
+
+GRANT ALL on paypaldata TO Clipper;
+GRANT ALL on paypaldata TO postgres;
